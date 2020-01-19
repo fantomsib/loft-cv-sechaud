17bcea96a8c1d6ebd3f7358f9b1031536e859484
Stanislav Sechaud <Stassechaud@gmail.com>
2020-01-19 19:39:25 +0100

    законченны все секции кроме секции скилов и адаптива для сладйреа с работами и перво й секции

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,4 @@
-console.log('this is skills module');
+frontend = { title: 'Fron-end', skills: { html: { title: 'HTML5', skillCount: 50 }, css: { title: 'CSS3', skillCount: 30 }, js: { title: 'javascript', skillCount: 70 }, vue: { title: 'jquery и Vue.js', skillCount: 70 } } }, workflow = {
+    title: 'Workflow',
+    skills: { git: { title: 'GIT', skillCount: 80 }, terminal: { title: 'Terminal', skillCount: 50 }, Gulp: { title: 'Gulp', skillCount: 10 }, webpack: { title: 'Webpack', skillCount: 30 } }
+}
